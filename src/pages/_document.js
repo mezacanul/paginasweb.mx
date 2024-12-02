46527ff630672ae305eccae6a979802196b69b99
Jose Eduardo Meza Canul <Lalo@192.168.1.77>
2024-12-02 13:05:42 -0600

    Changed title meta tag

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <title>Merivilla Co.</title>
+        <title>PaginasWeb Mx</title>
       </Head>
       <body>
         <Main />
